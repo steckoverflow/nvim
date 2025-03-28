1c6308fd0d8678364b981f56c7f0b6c733e247b3
Thomas Steck <thomas@steckoverflow.se>
2025-03-28 10:29:31 +0100

    no scrolling to the bottom

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- AstroCore provides a central place to modify mappings, vim options, autocommands, and more!
 -- Configuration documentation can be found with `:h astrocore`
 -- NOTE: We highly recommend setting up the Lua Language Server (`:LspInstall lua_ls`)
@@ -32,6 +30,8 @@ return {
         spell = false, -- sets vim.opt.spell
         signcolumn = "yes", -- sets vim.opt.signcolumn to yes
         wrap = false, -- sets vim.opt.wrap
+        scrolloff = 10,
+        sidescrolloff = 10,
       },
       g = { -- vim.g.<key>
         -- configure global vim variables (vim.g)
